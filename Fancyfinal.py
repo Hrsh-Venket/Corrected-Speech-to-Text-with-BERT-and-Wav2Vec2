917bb6233ef196d56bb2765cc82d246aaebd4fc1
Hrsh-Venket <hrsh.venket@gmail.com>
2023-04-28 17:50:58 +0530

    modified:   Fancyfinal.py

--- Fancyfinal.py
+++ Fancyfinal.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    st.write("yay, you are not an idiot")
